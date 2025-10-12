bd2a09223fcbc25188440413d90adc38d16a73b7
mikl14 <mikl.eli2002@gmail.com>
2025-10-12 22:13:35 +0300

    Внес правки по ТИ-2.3

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
 const skills = {
   data: [
-    { skill: "Java", level: 70, cssClass: "javaIcon", icon: "java.svg" },
-    { skill: "C#", level: 30, cssClass: "csharpIcon", icon: "Csharp.svg" },
-    { skill: "C++", level: 30, cssClass: "cppIcon", icon: "cpp.svg" },
-    { skill: "CSS", level: 5, cssClass: "cssIcon", icon: "css.svg" }
+    { skill: "Java", level: 70, icon: "java.svg" },
+    { skill: "C#", level: 30, icon: "Csharp.svg" },
+    { skill: "C++", level: 30, icon: "cpp.svg" },
+    { skill: "CSS", level: 5, icon: "css.svg" }
   ],
 
   generateList(parentElement) {
@@ -14,21 +14,16 @@ const skills = {
       const dd = document.createElement("dd");
       const div = document.createElement("div");
 
-      dt.classList.add(item.cssClass);
       dd.classList.add("progressBar");
       div.classList.add("progressBarInner");
 
       dt.textContent = item.skill;
       dt.style.backgroundImage = `url('icons/${item.icon}')`;
-      dt.style.backgroundRepeat = "no-repeat";
-      dt.style.backgroundPosition = "left center";
-      dt.style.paddingLeft = "40px";
 
       div.style.width = `${item.level}%`;
 
       dd.appendChild(div);
-      parentElement.appendChild(dt);
-      parentElement.appendChild(dd);
+      parentElement.append(dt,dd);
     });
   }
 };
